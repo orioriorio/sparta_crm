6c26b702f3fdd4fd38b1c6208e8b80f807d568ae
orihara <orihara.hosei@gmail.com>
2018-05-03 15:14:27 +0900

    Company model作成

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,7 @@
 class CustomersController < ApplicationController
+before_action :set_customer, only: [:show, :edit, :update, :destroy]
+before_action :set_companies, only: [:new, :create, :edit, :update]
+
   def index
     @customers = Customer.page(params[:page])
   end
@@ -17,11 +20,11 @@ class CustomersController < ApplicationController
   end
 
   def edit
-    @customer = Customer.find(params[:id])
+
   end
 
   def update
-    @customer = Customer.find(params[:id])
+
     if  @customer.update(customer_params)
       redirect_to @customer
     else
@@ -30,11 +33,10 @@ class CustomersController < ApplicationController
   end
 
   def show
-    @customer = Customer.find(params[:id])
   end
 
   def destroy
-    @customer = Customer.find(params[:id])
+
     @customer.destroy
     redirect_to customers_path
   end
@@ -42,6 +44,15 @@ class CustomersController < ApplicationController
   private
 
   def customer_params
-    params.require(:customer).permit(:family_name,:given_name,:email)
+    params.require(:customer).permit(:family_name,:given_name,:email,:company_id)
+  end
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+
+  def set_companies
+    @companies = Company.all
+
   end
 end
